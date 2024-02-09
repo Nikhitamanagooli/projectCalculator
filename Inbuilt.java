a0d8e8376d0be5e5e5683f1a1bb78956743e6a57
Nikhita <01fe22bca033@kletech.ac.in>
2024-02-09 14:20:43 +0530

    Program on inbult functions

---
+++ Inbuilt.java
@@ -0,0 +1,10 @@
+class Inbuilt{
+    public static void main(String[]args)
+    {
+        System.out.println("Maximum of the number = "+Math.max(5,3));
+        System.out.println("Minimun of the number = "+Math.min(5,3));
+        System.out.println("Square root of the number = "+Math.sqrt(5));
+        System.out.println("Cube root of the number = "+Math.cbrt(5));
+        System.out.println("Absolute of the number"+Math.abs(5));
+    }
+}
