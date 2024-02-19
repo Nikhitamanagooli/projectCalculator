f43095e3d4b8543363751e5dd895cedbb433fa34
Nikhitamanagooli <156498319+Nikhitamanagooli@users.noreply.github.com>
2024-02-19 15:10:36 +0530

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -13,26 +13,26 @@ class calculator {
         return num1/num2;
     }
     public static void main(String[] args) {
-        Scanner in=new Scanner(System.in);
-        System.out.println("Enter the num1: ");
-        int num1= in.nextInt();
-        System.out.println("Enter the num2: ");
-        int num2= in.nextInt();
+        // Scanner in=new Scanner(System.in);
+        // System.out.println("Enter the num1: ");
+        // int num1= in.nextInt();
+        // System.out.println("Enter the num2: ");
+        // int num2= in.nextInt();
         System.out.println("Enter the operation to be performed");
         System.out.println("1.Addition\n2.Subtraction\n3.Product\n4.Division\n");
         int ch= in.nextInt();
         switch (ch){
             case 1:
-                System.out.println("The sum of the "+num1+" and "+num2+" is: "+add(num1,num2));
+                System.out.println("The sum of the "+num1+" and "+num2+" is: "+add(2,6));
                 break;
             case 2:
-                System.out.println("The difference of the "+num1+" and "+num2+" is: "+sub(num1,num2));
+                System.out.println("The difference of the "+num1+" and "+num2+" is: "+sub(5,3));
                 break;
             case 3:
-                System.out.println("The Product of the "+num1+" and "+num2+" is: "+pro(num1,num2));
+                System.out.println("The Product of the "+num1+" and "+num2+" is: "+pro(3,2));
                 break;
             case 4:
-                System.out.println("The Product of the "+num1+" and "+num2+" is: "+div(num1,num2));
+                System.out.println("The Product of the "+num1+" and "+num2+" is: "+div(5,5));
                 break;
         }
     }
